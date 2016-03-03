5d83db9af8b0941edbe11c31822c390ef800b240
Masafumi Yokoyama <yokoyama@clear-code.com>
2016-03-03 15:51:33 +0900

    Don't include unused association in Groonga

--- db/seeds/development/credits.seeds.rb
+++ db/seeds/development/credits.seeds.rb
@@ -1,11 +1,10 @@
 after 'development:credit_roles', 'development:participants', 'development:posts' do
   site1 = Site.find_by!(name: 'site1')
-  participant = site1.participants.first!
   credit_role = site1.credit_roles.first!
 
-  site1.posts.published.order_by_recently.limit(3).each do |post|
+  site1.posts.published.order_by_recently.limit(10).each_with_index do |post, i|
     post.credits.create!(
-      participant: participant,
+      participant: site1.participants[i % 2],
       role: credit_role
     )
   end
