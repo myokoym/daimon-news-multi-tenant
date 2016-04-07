71b889670a580138f0b995de00c3019008a9b3f1
Masafumi Yokoyama <yokoyama@clear-code.com>
2016-04-07 14:04:25 +0900

    Add a script to generate posts

---
+++ scripts/generate_posts.rb
@@ -0,0 +1,50 @@
+site1 = Site.find_by!(name: "site1")
+site1.posts.create!(
+  title: "Hello",
+  body: <<-EOS.strip_heredoc,
+    # Hello
+    hi !
+
+    This is first post for site 1!
+  EOS
+  thumbnail: open(Rails.root.join("db/data/thumbnail.jpg")),
+  published_at: Time.current,
+  category: site1.categories.find_by!(slug: "category1"),
+)
+
+published_at = 3.minutes.from_now
+
+site1.posts.create!(
+  title: "This post will appear at #{published_at}",
+  body: <<-EOS.strip_heredoc,
+    # Hello
+    hi !
+
+    This post will appear at #{published_at}
+  EOS
+  thumbnail: open(Rails.root.join("db/data/thumbnail.jpg")),
+  published_at: published_at,
+  category: site1.categories.find_by!(slug: "category2"),
+)
+
+1000.times do |i|
+  i += 100
+
+  site1.posts.create!(
+    title: "Post #{i}",
+    body: <<~EOS,
+      page 1
+
+      <!--nextpage-->
+
+      page 2
+
+      <!--nextpage-->
+
+      page 3
+    EOS
+    thumbnail: open(Rails.root.join("db/data/thumbnail.jpg")),
+    published_at: Time.current,
+    category: site1.categories.find_by!(slug: "category1"),
+  )
+end
